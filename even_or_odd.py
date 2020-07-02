58714ad1b262fceffc9135d417b00c0cd36501d8
Sumitha Naidu <sumithanaidu1996@gmail.com>
2020-07-02 14:18:13 +0530

    Program to check Even or Odd

---
+++ even_or_odd.py
@@ -0,0 +1,8 @@
+#Enter the number from the user and depending on whether the number is even or
+#odd, print out an appropriate message to the user.
+
+num=int(input("Enter a Number : "))
+if(num%2==0):
+    print(num,"is an Even Number ")
+else:
+    print(num,"is a Odd Number ")
